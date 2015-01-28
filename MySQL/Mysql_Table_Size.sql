a1861d95e7416168d35d32d8a7badc560ccbded7
Luis Carbajo <luis.carbajo.23@gmail.com>
2015-01-28 16:53:57 +0100

    Table_size

---
+++ MySQL/Mysql_Table_Size.sql
@@ -0,0 +1,16 @@
+SELECT table_name AS "Tables", 
+round(((data_length + index_length) / 1024 / 1024), 2) "Size in MB" 
+FROM information_schema.TABLES 
+WHERE table_schema = "$DB_NAME"
+ORDER BY (data_length + index_length) DESC;
+
+
+
+
+
+
+SELECT table_name AS "Table", 
+round(((data_length + index_length) / 1024 / 1024), 2) "Size in MB" 
+FROM information_schema.TABLES 
+WHERE table_schema = "$DB_NAME"
+ AND table_name = "$TABLE_NAME";
